d3422b7242ff88c2c8ce1aa093e0f3d5306325f6
Andriani <theblue_squirrel@yahoo.com>
2025-04-07 01:10:24 -0400

    QR Code Generator  Finished

---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState, useRef } from "react";
+import QRCode from "react-qr-code";
+import { toPng } from "html-to-image";
+import { redirect } from "next/navigation";
+export default function HomePage() {
+  const [qrValue, setQrValue] = useState<string>("https://qrhuzz.com");
+  const qrRef = useRef(null);
+
+  const handleDownload = () => {
+    
+    if (qrRef.current !== null) {
+      console.log("QR ref", qrRef.current);
+      toPng(qrRef.current, { cacheBust: true })
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.download = `qrhuzz${Date.now()}.png`;
+          link.href = dataUrl;
+          link.click();
+          // redirect(dataUrl);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-start bg-gradient-to-b from-[#a3f7ac] to-[#008a0f] text-white md:justify-center">
+      <div className="mt-10 flex w-3/4 flex-col-reverse items-center justify-center gap-4 md:flex-col lg:w-2/5 lg:gap-12">
+        <h1 className="text-6xl font-extrabold tracking-tight text-white md:text-7xl">
+          QR Huzz
+        </h1>
+        <div className="flex w-full flex-row gap-4">
+          <Input
+            className="onfocus:translate-y-8 placeholder:text-white/80 focus:border-green-950 focus:ring-white"
+            placeholder="Enter link..."
+            onChange={(e) => setQrValue(e.target.value)}
+            type="url"
+            name="qrValue"
+          />
+        </div>
+        <div className="rounded-md bg-white p-4">
+          <QRCode
+            onClick={handleDownload}
+            style={{ cursor: "pointer" }}
+            size={256}
+            ref={qrRef}
+            value={qrValue}
+            className="rounded-md"
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
